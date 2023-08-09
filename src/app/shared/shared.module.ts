e75eff09418043d062987ede1025dfd81dce0156
ANDREE MATEO NARVAEZ ORTIZ <andreenarvaezmo@unimagdalena.edu.co>
2023-08-09 15:16:24 -0500

    routing básico

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,30 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NgModule } from '@angular/core';
+
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
 @NgModule({
   declarations: [
+    AboutPageComponent,
     HomePageComponent,
-    AboutPageComponent
+    SidebarComponent,
+    ContactPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  exports:[
+  exports: [
+    AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
-    AboutPageComponent
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
